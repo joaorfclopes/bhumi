0f26dbb0edb307effaffbc82b09cdca9f9e38aa8
Joao Lopes <joaorfclopes@gmail.com>
2019-10-20 14:33:56 +0100

    Header added

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,5 +1,8 @@
 import React from "react";
+import logo from "../assets/svg/logo.svg";
 
 export default function Header() {
-    return <div className="header"></div>
+  return <div className="header">
+      <img className="logo" src={logo} alt="logo"/>
+  </div>;
 }
