2d33971024ee383fe60c6143d51945a1974c4f3e
Joao Lopes <joaorfclopes@gmail.com>
2019-10-20 15:47:34 +0100

    Section1 added

--- src/components/Section1.jsx
+++ src/components/Section1.jsx
@@ -1,5 +1,45 @@
 import React from "react";
+import { useMediaQuery } from "react-responsive";
 
 export default function Section1() {
-  return <div className="section1"></div>;
+  const isDesktop = useMediaQuery({
+    query: "(min-device-width: 1224px)"
+  });
+  const isMobile = useMediaQuery({
+    query: "(max-device-width: 760px)"
+  });
+  return (
+    <div className="section1">
+      {isMobile && (
+        <>
+          <div className="title titleSection1 titleMobile">
+            <h3>[ The ultimate sophistication in high heels ]</h3>
+          </div>
+          <div className="textSection1 textMobile">
+            <p className="text">
+              A Bhumi concilia a tecnologia de ponta e a herança do know-how do
+              fabrico de calçado em Portugal para um conforto e performance
+              únicos em saltos altos, em peças vegan «eco-luxury»
+              personalizáveis, com uma linguagem estética de vanguarda.
+            </p>
+          </div>
+        </>
+      )}
+      {isDesktop && (
+        <>
+          <div className="title titleSection1 titleDesktop">
+            <h3>[ The ultimate sophistication in high heels ]</h3>
+          </div>
+          <div className="textSection1 textDesktop">
+            <p className="text">
+              A Bhumi concilia a tecnologia de ponta e a herança do know-how do
+              fabrico de calçado em Portugal para um conforto e performance
+              únicos em saltos altos, em peças vegan «eco-luxury»
+              personalizáveis, com uma linguagem estética de vanguarda.
+            </p>
+          </div>
+        </>
+      )}
+    </div>
+  );
 }
