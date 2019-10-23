a6c4fa9e6a9cf21a2d1802b3eb43ac88c048f876
João Lopes <joaolopes@Joaos-MacBook-Pro.local>
2019-10-23 09:51:16 +0100

    Section4 added

--- src/components/Section4.jsx
+++ src/components/Section4.jsx
@@ -1,5 +1,46 @@
 import React from "react";
+import { useMediaQuery } from "react-responsive";
+import shoe1 from "../assets/images/shoe1.jpg";
+import shoe2 from "../assets/images/shoe2.jpg";
+import shoe3 from "../assets/images/shoe3.jpg";
 
 export default function Section4() {
-  return <div className="section4"></div>;
+  const isDesktop = useMediaQuery({
+    query: "(min-device-width: 1224px)"
+  });
+  const isMobile = useMediaQuery({
+    query: "(max-device-width: 1224px)"
+  });
+  return (
+    <div className="container4">
+      {isMobile && (
+        <>
+          <div className="section4 section4Mobile">
+            <div className="shoes">
+              <img className="shoe" src={shoe1} alt="shoe1" />
+              <img className="shoe" src={shoe2} alt="shoe2" />
+              <img className="shoe" src={shoe3} alt="shoe3" />
+            </div>
+            <div className="divider"></div>
+            <div className="textSection4 textMobile">
+              <p className="text">
+                “The day when the upper echelon of luxury fashion proudly shows
+                off their ethical manufacturing methods, eco-fashion will become
+                irrisistible to consumers”
+              </p>
+              <p className="text secondaryText">
+                in “Luxury brands are silently going green. Is eco-fashion a
+                golden secret?” by Bunny Yan, The Squirrelz
+              </p>
+            </div>
+          </div>
+        </>
+      )}
+      {isDesktop && (
+        <>
+          <div className="section4 section3Desktop"></div>
+        </>
+      )}
+    </div>
+  );
 }
