d6314792f2af0b80bc0a078fc042cc1aa54a62a7
João Lopes <joaorfclopes@gmail.com>
2019-10-25 23:38:17 +0100

    Section1 added for desktop

--- src/components/Section1.jsx
+++ src/components/Section1.jsx
@@ -29,7 +29,19 @@ export default function Section1() {
       )}
       {isDesktop && (
         <>
-          <div className="section1 section1Desktop"></div>
+          <div className="section1 section1Desktop">
+            <div className="title titleSection1 titleDesktop">
+              <h3>[ The ultimate sophistication in high heels ]</h3>
+            </div>
+            <div className="textSection1 textDesktop">
+              <p className="text">
+                A Bhumi concilia a tecnologia de ponta e a herança do know-how
+                do fabrico de calçado em Portugal para um conforto e performance
+                únicos em saltos altos, em peças vegan «eco-luxury»
+                personalizáveis, com uma linguagem estética de vanguarda.
+              </p>
+            </div>
+          </div>
         </>
       )}
     </div>
