f4b503ab153152c6a8dd55a035502632047efc07
João Lopes <joaorfclopes@gmail.com>
2019-12-16 13:19:18 +0000

    Make some changes to make the website more responsive

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,8 +1,38 @@
 import React from "react";
 import logo from "../assets/svg/logo-footer.svg";
+import { useMediaQuery } from "react-responsive";
 
 export default function Footer() {
-  return <div className="footer">
-    <img className="logo-footer" src={logo} alt="logo-footer" />
-  </div>;
+  const isDesktop = useMediaQuery({
+    query: "(min-device-width: 1224px)"
+  });
+  const isMobile = useMediaQuery({
+    query: "(max-device-width: 1224px)"
+  });
+  return (
+    <div>
+      {isMobile && (
+        <>
+          <div className="footer footerMobile">
+            <img
+              className="logo-footer logo-footer-mobile"
+              src={logo}
+              alt="logo-footer"
+            />
+          </div>
+        </>
+      )}
+      {isDesktop && (
+        <>
+          <div className="footer footerDesktop">
+            <img
+              className="logo-footer logo-footer-desktop"
+              src={logo}
+              alt="logo-footer"
+            />
+          </div>
+        </>
+      )}
+    </div>
+  );
 }
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,8 +1,31 @@
 import React from "react";
 import logo from "../assets/svg/logo-header.svg";
+import { useMediaQuery } from "react-responsive";
 
 export default function Header() {
-  return <div className="header">
-      <img className="logo-header" src={logo} alt="logo-header"/>
-  </div>;
+  const isDesktop = useMediaQuery({
+    query: "(min-device-width: 1224px)"
+  });
+  const isMobile = useMediaQuery({
+    query: "(max-device-width: 1224px)"
+  });
+
+  return (
+    <div>
+      {isMobile && (
+        <>
+          <div className="header headerMobile">
+            <img className="logo-header-mobile" src={logo} alt="logo-header" />
+          </div>
+        </>
+      )}
+      {isDesktop && (
+        <>
+          <div className="header headerDesktop">
+            <img className="logo-header-desktop" src={logo} alt="logo-header" />
+          </div>
+        </>
+      )}
+    </div>
+  );
 }
